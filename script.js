01659563b0dec10430aeec4478759657cb98ab1d
aaron-might <aaronmighty7@gmail.com>
2021-09-16 17:17:45 -0400

    fix bgus

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todosUL = document.getElementById('todos');
+
 const todos = JSON.parse(localStorage.getItem('todos'));
 
-if (todos){
-    todos.forEach(todo =>{
+if (todos) {
+    todos.forEach((todo) => {
         addTodo(todo);
-    })
+    });
 }
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        addTodo();
-    });
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTodo();
+});
 
 function addTodo(todo) {
+    let todoText = input.value;
 
-    const todoText = input.value;
+    if (todo) {
+        todoText = todo.text;
+    }
 
     if (todoText) {
         const todoEl = document.createElement('li');
-        todoEl.innerText = todoText;
-        todos.appendChild(todoEl);
+        if (todo && todo.completed) {
+            todoEl.classList.add('completed');
+        }
 
+        todoEl.innerText = todoText;
+
+        // todos.appendChild(todoEl);
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle('completed');
 
@@ -36,12 +44,11 @@ function addTodo(todo) {
             updateLS();
         });
 
-        todosUL.apppendChild(todoEl);
+      todosUL.appendChild(todoEl);
 
         input.value = "";
         updateLS();
     }
-
 }
 
 function updateLS() {
